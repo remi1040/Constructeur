df8363f99e373b47e4b1cac42fbe3fcfde19ed32
remi1040 <48013030+remi1040@users.noreply.github.com>
2024-06-17 17:08:06 +0200

    Add files via upload

---
+++ Constructeur/Constructeur/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Constructeur
+{
+    internal class Building
+    {
+        public int NombreEtages { get; set; }
+        public int Hauteur { get; set; }
+        public Building(int nombreEtage, int hauteur) 
+        {
+            this.NombreEtages = nombreEtage;
+            this.Hauteur = hauteur;
+        }
+
+        public Building(int nombreEtage) 
+        {
+            this.NombreEtages = nombreEtage;
+            this.Hauteur = this.NombreEtages * 3;
+        }
+
+        public int GetFloorMaxSize()
+        {
+            return this.Hauteur / this.NombreEtages;
+        }
+
+        public int GetFloorCount() 
+        {
+            return this.NombreEtages;
+        }
+
+        public int GetSize() 
+        {
+            return this.Hauteur;
+        }
+
+    }
+}
---
+++ Constructeur/Constructeur/Program.cs
@@ -0,0 +1,14 @@
+﻿using Constructeur;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Building b1 = new Building(5,20);
+        Building b2 = new Building(10);
+
+        Console.WriteLine(b1.GetFloorMaxSize());
+        
+        Console.WriteLine(b2.GetSize());
+    }
+}
